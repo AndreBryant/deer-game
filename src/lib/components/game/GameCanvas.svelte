fd0b7e232f00186a6cc1e73c7ad1ff99e9e90ac8
embryzon <95128949+embryzon@users.noreply.github.com>
2024-10-20 15:44:54 +0800

    fix draw loop stopping for some crucial reason

--- src/lib/components/game/GameCanvas.svelte
+++ src/lib/components/game/GameCanvas.svelte
@@ -12,49 +12,56 @@
 
 	function draw(p5: any) {
 		p5.background(0);
-		console.log('SocketID', socketId);
-		const player = serverData.players[socketId];
+		if (serverData && serverData.players && serverData.players[socketId]) {
+			const player = serverData.players[socketId];
 
-		// FOR TESTING PLAYER CENTERED CAMERA
-		const t = translateCoords({
-			h: p5.height,
-			w: p5.width,
-			px: player.x,
-			py: player.y,
-			x: 0,
-			y: 0
-		});
-		p5.fill(25, 120, 13);
-		p5.stroke('white');
-		p5.strokeWeight(5);
-		p5.rect(t.x, t.y, 1000, 600);
-
-		p5.stroke(0);
-		p5.strokeWeight(1);
-		for (const data in serverData.players) {
-			const p = serverData.players[data];
-			if (data === socketId) {
-				p5.fill(player.color);
-				// fix name later
-				p5.text(player.name, p5.width / 2 - player.radius, p5.height / 2 - player.radius * 1.5);
-				p5.ellipse(p5.width / 2, p5.height / 2, player.radius * 2, player.radius * 2);
-				continue;
-			}
-
-			const pCoords = translateCoords({
+			// FOR TESTING PLAYER CENTERED CAMERA
+			const t = translateCoords({
 				h: p5.height,
 				w: p5.width,
 				px: player.x,
 				py: player.y,
-				x: p.x,
-				y: p.y
+				x: 0,
+				y: 0
 			});
+			p5.fill(25, 120, 13);
+			p5.stroke('white');
+			p5.strokeWeight(5);
+			p5.rect(t.x, t.y, 1000, 600);
 
-			p5.fill(p.color);
-			p5.text(p.name, pCoords.x - p.radius, pCoords.y - p.radius * 1.5);
-			p5.ellipse(pCoords.x, pCoords.y, player.radius * 2, player.radius * 2);
+			p5.stroke(0);
+			p5.strokeWeight(1);
+			for (const data in serverData.players) {
+				const p = serverData.players[data];
+				if (data === socketId) {
+					p5.fill(player.color);
+					// fix name later
+					p5.text(player.name, p5.width / 2 - player.radius, p5.height / 2 - player.radius * 1.5);
+					p5.ellipse(p5.width / 2, p5.height / 2, player.radius * 2, player.radius * 2);
+					continue;
+				}
+
+				const pCoords = translateCoords({
+					h: p5.height,
+					w: p5.width,
+					px: player.x,
+					py: player.y,
+					x: p.x,
+					y: p.y
+				});
+
+				p5.fill(p.color);
+				p5.text(p.name, pCoords.x - p.radius, pCoords.y - p.radius * 1.5);
+				p5.ellipse(pCoords.x, pCoords.y, player.radius * 2, player.radius * 2);
+			}
+		} else {
+			p5.push();
+			p5.fill(255);
+			p5.textSize(25);
+
+			p5.text('loading', p5.width / 2, p5.height / 2);
+			p5.pop();
 		}
-		// console.log(serverData.players);
 	}
 
 	function windowResized(p5: any) {
