7a50dc8812abadf640a0dafeab9df1c64f3e0c53
embryzon <95128949+embryzon@users.noreply.github.com>
2024-11-10 19:19:19 +0800

    update home page UI (korean singer) major changes

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-	import { Swords, SquarePlus, Fingerprint, Tag, CircleHelp, Copyright } from 'lucide-svelte';
+	import {
+		Swords,
+		SquarePlus,
+		Tag,
+		CircleHelp,
+		Copyright,
+		ChevronLeft,
+		Search,
+		User
+	} from 'lucide-svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 	import InputText from '$lib/components/ui/InputText.svelte';
 	import HomeBackground3 from '$lib/components/home/HomeBackground3.svelte';
@@ -8,7 +17,7 @@
 
 	const ws = io(import.meta.env.MODE === 'development' ? '' : import.meta.env.VITE_SERVER_URL);
 
-	let rooms: { [key: string]: { players: number; mapData: string } } = {};
+	let rooms: { [key: string]: { players: number; isGameStarted: boolean } } = {};
 	ws.on('rooms_updated', (data) => {
 		rooms = data;
 	});
@@ -17,34 +26,33 @@
 		rooms = data;
 	});
 
-	let gameID = '';
 	let username = '';
-
-	let joinValid = false;
-	$: joinValid = checkRoom(gameID) && checkUsername(username);
 
 	let createValid = false;
 	$: createValid = checkUsername(username);
 
-	let joinValidStatus: 'valid' | 'invalid' | 'none' = 'none';
-	$: joinValidStatus = checkRoom(gameID) ? 'valid' : 'invalid';
-	$: joinValidStatus = gameID.length <= 0 ? 'none' : checkRoom(gameID) ? 'valid' : 'invalid';
+	let joinValid = false;
+	$: joinValid = checkUsername(username);
+
+	let joinRoomPressed = false;
 
 	function checkUsername(username: string) {
 		return username.length > 0;
 	}
 
-	function checkRoom(gameID: string) {
-		return Boolean(rooms[gameID]);
-	}
+	let searchTerm = '';
 
 	$: rooms;
 
 	let numOfPlayers = '';
-	$: if (rooms[gameID]) {
-		numOfPlayers = rooms[gameID].players.toString();
-	} else {
-		numOfPlayers = '';
+	let filteredRooms = {};
+
+	$: {
+		const filtered = Object.entries(rooms).filter(
+			([gameID]) =>
+				gameID.toLowerCase().includes(searchTerm.toLowerCase()) && !rooms[gameID].isGameStarted
+		);
+		filteredRooms = Object.fromEntries(filtered);
 	}
 
 	onDestroy(() => ws?.disconnect());
@@ -67,51 +75,121 @@
 			<div>
 				<h1 class="mb-4 mt-8 text-center text-9xl">deer</h1>
 			</div>
-			<div class="mx-full flex flex-col gap-8">
-				<div class="h-4">
-					<InputText placeholder="Enter your name." iconLeft={Tag} bind:value={username} />
+			{#if !joinRoomPressed}
+				<div class="flex flex-col gap-8">
+					<div class="h-4 w-full">
+						<InputText placeholder="Enter your name." iconLeft={Tag} bind:value={username} />
+					</div>
+					<div class="flex h-12 w-80">
+						<div class="flex flex-col justify-center">
+							<Button
+								text="Join Game"
+								variant={joinValid ? 'primary' : 'disabled'}
+								iconLeft={SquarePlus}
+								onclick={() => (joinRoomPressed = true)}
+							/>
+						</div>
+						<div class="ml-12 flex w-[1px] flex-col">
+							<div class="w-full flex-grow bg-black"></div>
+							<div class="-ml-[6px] text-xs">OR</div>
+							<div class="w-full flex-grow bg-black"></div>
+						</div>
+						<div class="flex flex-grow items-center justify-center pl-4">
+							<Button
+								text="Create Game"
+								iconLeft={Swords}
+								variant={createValid ? 'primary' : 'disabled'}
+								href={`./game?host=true&username=${username}`}
+							/>
+						</div>
+					</div>
 				</div>
-				<div class="flex h-24">
-					<div class="mr-8 flex flex-grow basis-1/2 flex-col justify-center gap-4">
+				<div class="flex flex-col gap-4">
+					<Button text="How to Play" variant={'link'} iconLeft={CircleHelp} href={`./`} />
+					<Button text="Credits" variant={'link'} iconLeft={Copyright} href={`./`} />
+				</div>
+			{:else}
+				<div class="flex w-80 flex-col gap-4">
+					<div class="w-full">
 						<InputText
-							placeholder="Game ID here."
-							iconLeft={Fingerprint}
-							bind:value={gameID}
-							bind:valid={joinValidStatus}
+							placeholder="Search Game ID..."
+							iconLeft={Search}
+							bind:value={searchTerm}
 							bind:numOfPlayers
 						/>
-						<Button
-							text="Join Game"
-							variant={joinValid ? 'primary' : 'disabled'}
-							iconLeft={SquarePlus}
-							href={`./game?host=false&gameID=${gameID}&username=${username}`}
-						/>
 					</div>
-					<div class="mr-4 flex w-[1px] flex-col">
-						<div class="w-full flex-grow bg-black"></div>
-						<div class="-ml-2">OR</div>
-						<div class="w-full flex-grow bg-black"></div>
+					<div>
+						<h2 class="text-xs font-normal">
+							Joining as <span class="font-semibold">
+								{username.trim().length <= 0 ? '_blank' : username}
+							</span>
+						</h2>
 					</div>
-					<div class="ml-4 flex flex-grow basis-1/2 items-center">
+					<div
+						class="custom-scrollbar flex h-64 flex-col overflow-y-auto border-y border-black py-1 pr-2"
+					>
+						{#if Object.entries(filteredRooms).length > 0}
+							{#each Object.entries(filteredRooms) as [gameID, { players }]}
+								<div
+									class="flex w-full justify-between border-t border-black border-opacity-20 px-2 py-4 transition-all duration-75 hover:rounded-lg hover:border hover:bg-[#f5f5f5] hover:bg-opacity-20"
+								>
+									<div class="flex flex-col gap-2">
+										<div class="">
+											<span class="text-sm font-semibold">GameID: </span>
+											<span class="text-sm">
+												{gameID}
+											</span>
+										</div>
+										<div class="w-24 text-sm">
+											<Button
+												text="> join game"
+												variant={'primary'}
+												href={`./game?host=false&gameID=${gameID}&username=${username}`}
+											/>
+										</div>
+									</div>
+									<div class="flex flex-col">
+										<div class="flex justify-end gap-2 text-sm">
+											{players}<User size="16" />
+										</div>
+									</div>
+								</div>
+							{/each}
+						{:else}
+							<div class="text-sm">No rooms found...</div>
+						{/if}
+					</div>
+					<div>
 						<Button
-							text="Create Game"
-							iconLeft={Swords}
-							variant={createValid ? 'primary' : 'disabled'}
-							href={`./game?host=true&username=${username}`}
+							text="Back"
+							variant="link"
+							iconLeft={ChevronLeft}
+							onclick={() => (joinRoomPressed = false)}
 						/>
 					</div>
 				</div>
-			</div>
-		</section>
-		<section class="mx-4 flex flex-col gap-4">
-			<Button text="How to Play" variant={'link'} iconLeft={CircleHelp} href={`./`} />
-
-			<Button text="Credits" variant={'link'} iconLeft={Copyright} href={`./`} />
+			{/if}
 		</section>
 	</section>
 </main>
 
 <style>
+	.custom-scrollbar::-webkit-scrollbar {
+		width: 2px;
+	}
+
+	.custom-scrollbar::-webkit-scrollbar-track {
+		background: white;
+	}
+
+	.custom-scrollbar::-webkit-scrollbar-thumb {
+		background: rgba(0, 0, 0, 0.479);
+	}
+
+	.custom-scrollbar::-webkit-scrollbar-thumb:hover {
+		background: red;
+	}
+
 	.custom-animation {
 		animation: float 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) infinite;
 		backface-visibility: hidden;
