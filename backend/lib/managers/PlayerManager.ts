99261b515889c58262d553f7a1798a014ed8f58b
embryzon <95128949+embryzon@users.noreply.github.com>
2024-11-23 19:28:56 +0800

    change attack duration from 250 to 500

--- backend/lib/managers/PlayerManager.ts
+++ backend/lib/managers/PlayerManager.ts
@@ -173,7 +173,7 @@ export class PlayerManager {
 
 		player.action = 'attack';
 		player.actionStartTime = Date.now();
-		player.actionEndTime = Date.now() + 250;
+		player.actionEndTime = Date.now() + 500;
 		const targets = this.getPlayersInRoom(player.room);
 		for (const target of Object.values(targets)) {
 			if (target.isDead) continue;
